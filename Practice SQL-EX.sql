afa67f9b81c93039df93ec4a4476b1e100d60ccb
Vitaliy <fruitamin@yandex.ru>
2022-09-20 00:08:08 +0300

    Решены новые задачи

--- Practice SQL-EX.sql
+++ Practice SQL-EX.sql
@@ -57,3 +57,55 @@ WHERE `type` = 'PC' AND `maker` NOT IN (
   FROM `product`
   WHERE `type` = 'Laptop'
 )
+
+--9
+SELECT DISTINCT `maker`
+FROM `product`
+INNER JOIN `pc` `pc` USING(`model`)
+WHERE `pc`.`speed` >= 450
+
+-- 10
+SELECT `model`, `price`
+FROM `printer`
+WHERE `price` IN (
+  SELECT MAX(`price`)
+  FROM `printer`
+)
+
+-- 11
+SELECT AVG(`speed`)
+FROM `pc`
+
+-- 12
+SELECT AVG(`speed`)
+FROM `laptop`
+WHERE `price` > 1000
+
+-- 13
+SELECT AVG(`speed`)
+FROM `pc`
+INNER JOIN `product` `p` USING(`model`)
+WHERE `p`.`maker` = 'A'
+
+-- 14
+SELECT `s`.`class`, `s`.`name`, `c`.`country`
+FROM `Ships` `s`
+INNER JOIN `Classes` `c` USING(`class`)
+WHERE `c`.`numGuns` >= 10
+
+-- 15
+SELECT `hd`
+FROM `pc`
+GROUP BY `hd`
+HAVING COUNT(`hd`) > 1
+
+-- 16
+SELECT DISTINCT p1.model, p2.model, p1.speed, p1.ram
+FROM pc p1, pc p2
+WHERE p1.speed = p2.speed AND p1.ram = p2.ram AND p1.model > p2.model
+
+-- 17
+SELECT DISTINCT `p`.`type`, `p`.`model`, `l`.`speed`
+FROM `laptop` `l`
+JOIN `product` `p` USING(`model`)
+WHERE `l`.`speed` < (SELECT MIN(speed) FROM `pc`)
